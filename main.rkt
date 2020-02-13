5bbd31c8a622954d5338e9ade4a3e11f3a070b75
Stephen Foster <stephen@thoughtstem.com>
2020-02-13 13:14:03 -0800

    function call added by another dev

--- main.rkt
+++ main.rkt
@@ -1,6 +1,11 @@
 #lang racket/base
 
-(provide function-from-demo-package-a)
+(provide 
+  function-from-demo-package-a
+  2nd-function-from-demo-package-a)
 
 (define (function-from-demo-package-a)
   (displayln "I am from demo package A!!"))
+
+(define (2nd-function-from-demo-package-a)
+  (displayln "I am from demo package A!! (2nd function)"))
