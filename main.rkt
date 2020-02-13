72afcde54b7641eb4335a946ff051d07c16028ff
Stephen Foster <stephen@thoughtstem.com>
2020-02-13 13:26:13 -0800

    new function definition

--- main.rkt
+++ main.rkt
@@ -2,10 +2,14 @@
 
 (provide 
   function-from-demo-package-a
-  2nd-function-from-demo-package-a)
+  2nd-function-from-demo-package-a
+  3rd-function-from-demo-package-a)
 
 (define (function-from-demo-package-a)
   (displayln "I am from demo package A!!"))
 
 (define (2nd-function-from-demo-package-a)
   (displayln "I am from demo package A!! (2nd function)"))
+
+(define (3rd-function-from-demo-package-a)
+  (displayln "I am from demo package A!! (3rd function)"))
